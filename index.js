dbbd1b239af906203a24e140ea72c48ab1a6feeb
sofi122d <sofialivnorre@gmail.com>
2021-11-30 13:19:44 +0100

    main 1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+
+// hver eneste gang man skriver noget i bodyen, så ksal det laves om fra en tekststreng til et JSON objekt
 app.use(express.json());
 
 // installering af PORT
@@ -7,3 +9,13 @@ const PORT= 1300
 app.listen(PORT, () => {
     console.log('Server is listening on PORT ' + PORT)
 });
+
+// Middleware - Det man definere der sker før hver eneste forespørgsel - Views ligger som endpoints
+app.use(express.static("./src/views"));
+
+
+// Controllers - Hent den fil jeg har her, og læg den ind i variablen i controllerfilen
+const brugerController= require("./src/controllers/bruger-controller");
+
+// Routes -- 
+app.use("/brugere", brugerController);
